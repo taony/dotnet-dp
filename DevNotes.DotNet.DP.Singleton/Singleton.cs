a2efad4888e05392a3c419a8ee460b0307d32840
taony <taonyzhang@gmail.com>
2016-05-18 21:27:44 +0800

    commit

--- DevNotes.DotNet.DP.Singleton/Singleton.cs
+++ DevNotes.DotNet.DP.Singleton/Singleton.cs
@@ -1,18 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DevNotes.DotNet.DP.Singleton
+﻿namespace DevNotes.DotNet.DP.Singleton
 {
     /// <summary>
     /// 单体模式一
     /// </summary>
     public class Singleton
     {
+        //私有，静态实例
         private static Singleton _instance = null;
 
+        //私有构造函数
+        private Singleton()
+        {
+
+        }
+
+        //静态工厂方法
         public static Singleton CreateInstance()
         {
             return _instance ?? (_instance = new Singleton());
