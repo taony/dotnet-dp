3e9c1a510cdd6a80671a0f9516bc842e96504266
taony <taonyzhang@gmail.com>
2016-05-18 21:29:20 +0800

    commit

---
+++ DevNotes.DotNet.DP.Singleton/SingletonForLazy .cs
@@ -0,0 +1,20 @@
+﻿namespace DevNotes.DotNet.DP.Singleton
+{
+    /// <summary>
+    /// 延迟实例化的单体模式
+    /// </summary>
+    public class SingletonForLazy
+    {
+        //初始暂不实例化
+        private static SingletonForLazy _instance = null;
+
+        //私有的构造子(构造器,构造函数,构造方法)
+        private SingletonForLazy() { }
+
+        //静态的工厂方法，需要使用时才去创建该单体
+        public static SingletonForLazy GetInstance()
+        {
+            return _instance ?? (_instance = new SingletonForLazy());
+        }
+    }
+}
---
+++ DevNotes.DotNet.DP.Singleton/SingletonForThread.cs
@@ -0,0 +1,44 @@
+﻿namespace DevNotes.DotNet.DP.Singleton
+{
+    /// <summary>
+    /// 线程安全的单体模式
+    /// </summary>
+    public class SingletonForThread
+    {
+        /// <summary>
+        /// 单体实例
+        /// </summary>
+        private static SingletonForThread _instance;
+        /// <summary>
+        /// 线程安全锁
+        /// </summary>
+        private static object _lock = new object();
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        private SingletonForThread()
+        {
+        }
+
+        /// <summary>
+        /// 获取流程实例
+        /// </summary>
+        /// <returns></returns>
+        public static SingletonForThread GetInstance()
+        {
+            //双重锁方式较好地解决了多线程下的单例模式实现
+            if (null == _instance)
+            {
+                lock (_lock)
+                {
+                    if (null == _instance)
+                    {
+                        _instance = new SingletonForThread();
+                    }
+                }
+            }
+            return _instance;
+        }
+    }
+}
