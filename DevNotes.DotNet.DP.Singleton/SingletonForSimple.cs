e7d128fbc9ddca63717b1304398d64437035bc4a
taony <taonyzhang@gmail.com>
2016-05-18 21:50:38 +0800

    simple singleton

---
+++ DevNotes.DotNet.DP.Singleton/SingletonForSimple.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevNotes.DotNet.DP.Singleton
+{
+    public class SingletonForSimple
+    {
+        private SingletonForSimple()
+        {
+        }
+
+        public static readonly SingletonForSimple instance = new SingletonForSimple();
+    }
+}
