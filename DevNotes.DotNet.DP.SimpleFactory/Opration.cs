9f74f2dc8175f2fe75442a58616350c30082171d
taony <taonyzhang@gmail.com>
2016-05-21 23:34:22 +0800

    sample factory

---
+++ DevNotes.DotNet.DP.SimpleFactory/Opration.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevNotes.DotNet.DP.SimpleFactory
+{
+    class Opration
+    {
+        public double NumberA { get; set; }
+        public double NumberB { get; set; }
+    }
+}
