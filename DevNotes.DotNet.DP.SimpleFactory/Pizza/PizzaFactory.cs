1ae620abb6f6fdbacc3f42de31f17c9bec46427c
taony <taonyzhang@gmail.com>
2016-05-26 23:53:01 +0800

    SF for Pizza

---
+++ DevNotes.DotNet.DP.SimpleFactory/Pizza/PizzaFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevNotes.DotNet.DP.SimpleFactory.Pizza
+{
+    public class PizzaFactory
+    {
+        public static Pizza CreatePizza(string type)
+        {
+            Pizza pizza = null;
+            switch (type)
+            {
+                case "cheese":
+                    pizza = new CheesePizza();
+                    break;
+
+            }
+
+            return pizza;
+        }
+    }
+}
