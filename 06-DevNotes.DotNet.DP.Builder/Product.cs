5bb7ba8ad74da71d4b7b471ddc6cdbeb8ce14650
taony <taonyzhang@gmail.com>
2016-07-08 00:16:09 +0800

    Builder

---
+++ 06-DevNotes.DotNet.DP.Builder/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevNotes.DotNet.DP.Builder
+{
+    public abstract class Product
+    {
+
+    }
+}
