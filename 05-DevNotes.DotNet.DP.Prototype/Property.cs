ff5161778e21e7a66d13293f736d523f648e32ea
taony <taonyzhang@gmail.com>
2016-07-08 23:49:57 +0800

    Property

---
+++ 05-DevNotes.DotNet.DP.Prototype/Property.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevNotes.DotNet.Prototype
+{
+    /// <summary>
+    /// 原型
+    /// </summary>
+    public abstract class Property
+    {
+        /// <summary>
+        /// 克隆
+        /// </summary>
+        /// <returns></returns>
+        public abstract Property Clone();
+    }
+}
