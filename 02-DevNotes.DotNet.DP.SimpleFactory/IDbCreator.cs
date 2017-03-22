80647fd9c94534e1b4dcecf766d0347311e9bcf2
taony <taonyzhang@gmail.com>
2017-03-22 23:30:05 +0800

    f

--- 02-DevNotes.DotNet.DP.SimpleFactory/IDbCreator.cs
+++ 02-DevNotes.DotNet.DP.SimpleFactory/IDbCreator.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DevNotes.DotNet.DP.SimpleFactory
+﻿namespace DevNotes.DotNet.DP.SimpleFactory
 {
     /// <summary>
     /// 定义创建对象接口
